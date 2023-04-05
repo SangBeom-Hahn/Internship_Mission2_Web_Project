ce402b0ca38b23617317040da57035ce04511434
SangBeom-Hahn <hsb990917@gmail.com>
2023-04-05 15:21:42 +0900

    Initial commit

---
+++ src/main/java/internship/intern_web_project/InternWebProjectApplication.java
@@ -0,0 +1,13 @@
+package internship.intern_web_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternWebProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternWebProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/internship/intern_web_project/InternWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package internship.intern_web_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternWebProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
